07343f43b6d739c960b4be7cefe18b1291d8566f
S Maher <stuart.maher@outlook.com>
2021-12-01 08:06:17 +0000

    Add project files.

---
+++ AOC2021_Day1/Program.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.IO;
+
+namespace AOC2021_Day1
+{
+    class Program
+    {
+       
+        static void Main(string[] args)
+        {
+            int increaseTotal = 0;
+            int threeMeasureTotal = 0;
+            string[] measuresString = File.ReadAllLines("Input.txt");
+            int[] measures = new int[measuresString.Length];
+            int[] threemeasure = new int[measuresString.Length - 2];
+            for (int measureCount = 0; measureCount < measuresString.Length; measureCount++)
+            {
+                measures[measureCount] = Convert.ToInt32(measuresString[measureCount]);
+          
+                if(measureCount> 0)
+                {
+                    if(measures[measureCount] > measures[measureCount-1])
+                    {
+                        increaseTotal++;
+                    }
+                }
+            }
+
+            for (int measureCount = 0; measureCount < threemeasure.Length; measureCount++)
+            {
+              
+                    threemeasure[measureCount] = measures[measureCount] + measures[measureCount + 1] + measures[measureCount + 2];
+                if (measureCount > 0)
+                {
+                    if (threemeasure[measureCount] > threemeasure[measureCount - 1])
+                    {
+                        threeMeasureTotal++;
+                    }
+                }
+
+            }
+            
+            Console.WriteLine(increaseTotal);
+            Console.WriteLine(threeMeasureTotal);
+        }
+    }
+}
