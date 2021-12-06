956420605e60d914be230c82bae8e7f96667265a
S Maher <Mahers@hattonacademy.org.uk>
2021-12-06 15:55:23 +0000

    Day 5 & 6

---
+++ AOC2021_Day5/Program.cs
@@ -0,0 +1,92 @@
+﻿using System;
+using System.IO;
+
+namespace AOC2021_Day5
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int size = 1000;
+            string[] Puzzleinput = File.ReadAllLines("Input.txt");
+
+            int[,] VentGrid = new int[size, size];
+
+            foreach (string vent in Puzzleinput)
+            {
+                string Coords1 = vent.Split(" -> ")[0];
+                string Coords2 = vent.Split(" -> ")[1];
+
+                int x1 = Convert.ToInt32(Coords1.Split(",")[0]);
+                int x2 = Convert.ToInt32(Coords2.Split(",")[0]);
+                int y1 = Convert.ToInt32(Coords1.Split(",")[1]);
+                int y2 = Convert.ToInt32(Coords2.Split(",")[1]);
+
+                if(y1 == y2)
+                {
+                    for (int xCount = Math.Min(x1, x2); xCount <= Math.Max(x1, x2); xCount++)
+                    {
+                        VentGrid[xCount, y1]++;
+                    }
+                }
+                else if (x1 == x2)
+                {
+                    for (int yCount = Math.Min(y1, y2); yCount <= Math.Max(y1, y2); yCount++)
+                    {
+                        VentGrid[x1, yCount]++;
+                    }
+                }
+                else
+                {
+                    
+                    int offsetX = 0;
+                    int offsetY = 0;
+                    for (int xCount = Math.Min(x1, x2); xCount <= Math.Max(x1, x2); xCount++)
+                    {
+                        VentGrid[x1 + offsetX, y1 + offsetY]++;
+                        if(x1 > x2)
+                        {
+                            offsetX--;
+                        }
+                        else
+                        {
+                            offsetX++;
+                        }
+                        if (y1 > y2)
+                        {
+                            offsetY--;
+                        }
+                        else
+                        {
+                            offsetY++;
+                        }
+                    }
+                }
+
+            }
+            int total = 0;
+            for (int xCount = 0; xCount < size; xCount++)
+            {
+                for (int yCount = 0; yCount < size; yCount++)
+                {
+                    if( VentGrid[xCount, yCount] > 1)
+                    {
+                        total++;
+                    }
+                }
+            }
+            Console.WriteLine(total);
+            if (size == 10)
+            {
+                for (int xCount = 0; xCount < size; xCount++)
+                {
+                    for (int yCount = 0; yCount < size; yCount++)
+                    {
+                        Console.Write(VentGrid[xCount, yCount]);
+                    }
+                    Console.WriteLine();
+                }
+            }
+        }
+    }
+}
---
+++ AOC2021_Day6/Program.cs
@@ -0,0 +1,84 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+
+namespace AOC2021_Day6
+{
+
+    class LanternFish
+    {
+        private int age;
+
+        public int Age { get => age; set => age = value; }
+    }
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string Puzzleinput = "1,1,1,1,1,1,1,4,1,2,1,1,4,1,1,1,5,1,1,1,1,1,1,1,1,1,1,1,1,5,1,1,1,1,3,1,1,2,1,2,1,3,3,4,1,4,1,1,3,1,1,5,1,1,1,1,4,1,1,5,1,1,1,4,1,5,1,1,1,3,1,1,5,3,1,1,1,1,1,4,1,1,1,1,1,2,4,1,1,1,1,4,1,2,2,1,1,1,3,1,2,5,1,4,1,1,1,3,1,1,4,1,1,1,1,1,1,1,4,1,1,4,1,1,1,1,1,1,1,2,1,1,5,1,1,1,4,1,1,5,1,1,5,3,3,5,3,1,1,1,4,1,1,1,1,1,1,5,3,1,2,1,1,1,4,1,3,1,5,1,1,2,1,1,1,1,1,5,1,1,1,1,1,2,1,1,1,1,4,3,2,1,2,4,1,3,1,5,1,2,1,4,1,1,1,1,1,3,1,4,1,1,1,1,3,1,3,3,1,4,3,4,1,1,1,1,5,1,3,3,2,5,3,1,1,3,1,3,1,1,1,1,4,1,1,1,1,3,1,5,1,1,1,4,4,1,1,5,5,2,4,5,1,1,1,1,5,1,1,2,1,1,1,1,1,1,1,1,1,1,2,1,1,1,1,1,1,5,1,1,1,1,1,1,3,1,1,2,1,1";
+
+            UInt64[] FishDays = new UInt64[9];
+
+            string[] startingAges = Puzzleinput.Split(",");
+            foreach (string startingFish in startingAges)
+            {
+
+                FishDays[Convert.ToInt32(startingFish)]++;
+
+            }
+            for (int dayCount = 0; dayCount < 256; dayCount++)
+            {
+                UInt64 newFishes = FishDays[0];
+                FishDays[0] = FishDays[1];
+                FishDays[1] = FishDays[2];
+                FishDays[2] = FishDays[3];
+                FishDays[3] = FishDays[4];
+                FishDays[4] = FishDays[5];
+                FishDays[5] = FishDays[6];
+                FishDays[6] = FishDays[7];
+                FishDays[7] = FishDays[8];
+
+                FishDays[8] = newFishes;
+                FishDays[6] += newFishes;
+
+
+            }
+            Console.WriteLine(FishDays[0] + FishDays[1] + FishDays[2] + FishDays[3] + FishDays[4] + FishDays[5] + FishDays[6] + FishDays[7] + FishDays[8]);
+
+            //string Puzzleinput = "3,4,3,1,2";
+            /*
+            List<LanternFish> LanterFishies = new List<LanternFish>();
+            string[] startingAges = Puzzleinput.Split(",");
+            foreach (string startingFish in startingAges)
+            {
+
+                    LanterFishies.Add(new LanternFish() { Age = Convert.ToInt32(startingFish) });
+
+            }
+
+            for (int dayCount = 0; dayCount < 256; dayCount++)
+            {
+                List<LanternFish> newFish = new List<LanternFish>();
+                foreach (LanternFish Fish in LanterFishies)
+                {
+                    Fish.Age--;
+                    if(Fish.Age < 0)
+                    {
+                        newFish.Add(new LanternFish { Age = 8 });
+                        Fish.Age = 6;
+                    }
+                }
+                LanterFishies.AddRange(newFish);
+
+               Console.WriteLine("After " + dayCount + "days ");
+                /*    foreach (LanternFish Fish in LanterFishies)
+                   {
+                       Console.Write(Fish.Age + ",");
+                   }
+                   Console.WriteLine();
+            }
+
+            Console.WriteLine(LanterFishies.Count); */
+        }
+    }
+}
