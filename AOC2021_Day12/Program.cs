70e200cf59578cb9385320a1d90b66195942db5c
S Maher <Mahers@hattonacademy.org.uk>
2021-12-14 07:35:55 +0000

    Day 12 and 13

---
+++ AOC2021_Day12/Program.cs
@@ -0,0 +1,113 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+
+namespace AOC2021_Day12
+{
+    public class Node
+    {
+        private string name;
+        private List<Node> links = new List<Node>();
+        private bool lower;
+
+        public string Name
+        {
+            get => name;
+            set
+            {
+                name = value;
+             
+                    
+                    if (name.Equals(name.ToLower()))
+                    {
+                        lower = true;
+                    }
+                    else
+                    {
+                        lower = false;
+                    }
+                }
+            
+        }
+        public List<Node> Links { get => links; set => links = value; }
+        public bool Lower { get => lower; }
+    }
+   
+    class Program
+    {
+        static Dictionary<string, Node> Graph;
+        static List<Node> Route = new List<Node>();
+        static int routeCount = 0;
+        static Node twice = null;
+        static void Main(string[] args)
+        {
+            Graph = new Dictionary<string, Node>();
+            string[] Puzzleinput = File.ReadAllLines("Input.txt");
+            foreach (string line in Puzzleinput)
+            {
+                string Start = line.Split("-")[0];
+                string End = line.Split("-")[1];
+                if(!Graph.ContainsKey(Start))
+                {
+                    Node newNode = new Node() { Name = Start };
+                    Graph.Add(Start, newNode);
+                }
+                if (!Graph.ContainsKey(End))
+                {
+                    Node newNode = new Node() { Name = End };
+                    Graph.Add(End, newNode);
+                }
+                Graph[Start].Links.Add(Graph[End]); 
+                Graph[End].Links.Add(Graph[Start]);
+            }
+
+            FindRoute(Graph["start"]);
+            Console.WriteLine(routeCount);
+            
+        }
+
+        private static void FindRoute(Node From)
+        {
+
+
+            if (From.Name == "end")
+            {
+                routeCount++;
+               /* foreach (Node node in Route)
+                {
+                    Console.Write(node.Name + "-");
+                }
+                Console.Write("end");
+                Console.WriteLine();*/
+            }
+
+            else
+            {
+                Route.Add(From);
+                foreach (Node searchNode in From.Links)
+                {
+                    if (!searchNode.Lower || !Route.Contains(searchNode))
+                    {
+                        FindRoute(searchNode);
+                    }
+                    else if (searchNode.Lower && twice == null)
+                    {
+                        if (!searchNode.Name.Equals("start") && !searchNode.Equals("end"))
+                        {
+                            twice = searchNode;
+                            FindRoute(searchNode);
+                        }
+                    }
+                }
+
+                Route.Remove(From);
+                if (From == twice)
+                {
+                    twice = null;
+                }
+
+            }
+            
+        }
+    }
+}
---
+++ AOC2021_Day13/Program.cs
@@ -0,0 +1,149 @@
+﻿using System;
+using System.IO;
+
+namespace AOC2021_Day13
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            char[,] Grid;
+            string[] Puzzleinput = File.ReadAllLines("Input.txt");
+
+            int max = 0;
+            foreach (string line in Puzzleinput)
+            {
+                if (line != "" && line[0] != 'f')
+                {
+                    if (max < Convert.ToInt32(line.Split(",")[0]))
+                    {
+                        max = Convert.ToInt32(line.Split(",")[0]);
+                    }
+                    if (max < Convert.ToInt32(line.Split(",")[1]))
+                    {
+                        max = Convert.ToInt32(line.Split(",")[1]);
+                    }
+                }
+            }
+            max++;
+
+            Grid = new char[max, max];
+            foreach (string line in Puzzleinput)
+            {
+                if (line != "" && line[0] != 'f')
+                {
+                    int x = Convert.ToInt32(line.Split(",")[0]);
+                    int y = Convert.ToInt32(line.Split(",")[1]);
+                    Grid[x, y] = '*';
+                }
+                else if (line == "")
+                {
+
+                }
+                else
+                {
+                    int foldLine = Convert.ToInt32(line.Split("=")[1]);
+                  
+                        Fold(Grid, line[11], foldLine);
+                    
+                    int dotCount = 0;
+                    for (int xCount = 0; xCount < max; xCount++)
+                    {
+                        for (int yCount = 0; yCount < max; yCount++)
+                        {
+                            if(Grid[xCount, yCount] == '*')
+                            {
+                                dotCount++;
+                            }
+                        }
+                    }
+                    Console.WriteLine(dotCount);
+                }
+            }
+
+            int xmin = -1, ymin = -1, xmax = max, ymax = max;
+
+            int xpos = 0;
+            while (xmin == -1)
+            {
+                for (int ycount = 0; ycount < max; ycount++)
+                {
+                    if(Grid[xpos, ycount] == '*')
+                    {
+                        xmin = xpos;
+                    }
+                }
+                xpos++;
+            }
+            int ypos = 0;
+            while (ymin == -1)
+            {
+                for (int xcount = 0; xcount < max; xcount++)
+                {
+                    if (Grid[xcount, ypos] == '*')
+                    {
+                        ymin = ypos;
+                    }
+                }
+                ypos++;
+            }
+            xpos = max - 1;
+            while (xmax == max)
+            {
+                for (int ycount = 0; ycount < max; ycount++)
+                {
+                    if (Grid[xpos, ycount] == '*')
+                    {
+                        xmax = xpos;
+                    }
+                }
+                xpos--;
+            }
+            ypos = max - 1;
+            while (ymax == max)
+            {
+                for (int xcount = 0; xcount < max; xcount++)
+                {
+                    if (Grid[xcount, ypos] == '*')
+                    {
+                        ymax = ypos;
+                    }
+                }
+                ypos--;
+            }
+
+            for (int xCount = xmin; xCount <= xmax; xCount++)
+            {
+                for (int yCount = xmin; yCount <= ymax; yCount++)
+                {
+                    if (Grid[xCount, yCount] == '*')
+                    {
+                        Console.Write(Grid[xCount, yCount]);
+                    }
+                    else
+                    {
+                        Console.Write(" ");
+                    }
+                }
+                Console.WriteLine();
+            }
+        }
+
+        private static void Fold(char[,] Grid, char dim, int foldLine)
+        {
+            int max = Grid.GetLength(0);
+
+            for (int xCount = dim == 'x' ? foldLine + 1 : 0; xCount < max; xCount++)
+            {
+                for (int yCount = dim == 'y'? foldLine + 1:0; yCount < max; yCount++)
+                {
+                    if (Grid[xCount, yCount] == '*')
+                    {
+                        Grid[dim == 'x' ? foldLine - (xCount - foldLine) : xCount, dim == 'y' ? foldLine - (yCount - foldLine) : yCount] = '*';
+                        Grid[xCount, yCount] = ' ';
+                    }
+                }
+            }
+        }
+    }
+}
